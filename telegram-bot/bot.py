663ee3ca3fa0993fde151345707ff52d45291d8e
DESKTOP-2AVS6OD\Alex <alexanderkudin26@gmail.com>
2022-05-23 17:59:15 +0300

    FIx some misstakes

--- telegram-bot/bot.py
+++ telegram-bot/bot.py
@@ -1,7 +1,16 @@
-import telebot
-token = '5188285015:AAEVPBMesbRY4jhnly7d1NRt6mPg0R8Df1U'
-bot=telebot.TeleBot(token)
+import telebot, random
+token='5387244419:AAHaQZqRDnPdU9qo4zy3CKzgeefZT09qIfw'
+bot = telebot.TeleBot(token)
+global spisok
+spisok = []
 @bot.message_handler(commands=['start'])
-def start_message(message):
-    bot.send_message(message.chat.id,'Привет')
+def hello(message):
+    msg = bot.send_message(message.chat.id,'Вводите')
+    spisok.append(msg.text)
+    if msg.text != '/go':
+        bot.register_next_step_handler(msg, hello)
+@bot.message_handler(commands=['go'])
+def opros(message):
+    msg = bot.send_message(message.chat.id,random.choice(spisok))
+    bot.register_next_step_handler(msg, opros)
 bot.polling(none_stop=True)
